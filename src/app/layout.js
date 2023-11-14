4d71a19280168aca927b8f0424bc762037fb34f7
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-11-14 19:26:54 +0600

    navbar added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar/Navbar";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+        <Navbar />
+        <main className="max-w-5xl mx-auto p-4">{children}</main>
+      </body>
     </html>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="max-w-5xl mx-auto p-4">
+      <header className="flex items-center justify-between">
+        <Link href="/" className="text-primary font-semibold text-2xl">
+          Bite Zen
+        </Link>
+
+        <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+          <Link href="/" className="active:text-primary">
+            Home
+          </Link>
+          <Link href="">Menu</Link>
+          <Link href="">About</Link>
+          <Link href="">Contact</Link>
+          <Link href="" className="bg-primary px-6 py-2 rounded-lg text-white">
+            Login
+          </Link>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
